479d375e8bd70cbcefee51d30c7f51b34eb2e76a
Kelvin JC <ojkelvinchu@gmail.com>
2022-10-07 09:55:05 +0100

    new model for bug

--- src/bugstack/models.py
+++ src/bugstack/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 # Create your models here.
 class Bug(models.Model):
     name = models.CharField(max_length=100)
-    #programming_language = models.CharField(max_length=100)
-   #project = models.CharField(max_length=50)
+    programming_language = models.CharField(max_length=100)
+    project = models.CharField(max_length=50)
     time_created = models.DateTimeField(default=timezone.now)
-    #is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
